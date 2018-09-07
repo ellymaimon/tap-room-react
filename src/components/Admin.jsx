664ec978b29c47a05542b14a94c9ef983f7ac0a9
ellymaimon <eliranmaimon@gmail.com>
2018-09-07 12:29:45 -0700

    Add link to new keg form as button to admin component

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import KegList from './KegList';
+import { Link } from "react-router-dom";
 
 function Admin(){
   return (
     <div>
-      <h1>Welcome, employee!</h1>
+      <h2>Welcome, employee!</h2>
+      <Link Link to="/newkeg"><button>Add Keg</button></Link>
+      <h1>Kegs Available:</h1>
       <KegList/>
     </div>
   );
