a2b50aa162ba75155473a2c319f2434bd0ade7ad
ellymaimon <eliranmaimon@gmail.com>
2018-09-07 10:56:41 -0700

    Add content to keg list component, including imported mock data

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { KEGLIST } from './MockData';
 
 function KegList() {
+  console.log(KEGLIST);
+  
   return (
     <div>
-      
+      <p>{KEGLIST.masterKegList[0].name}</p>
     </div>
-  )
+  );
 }
+
+export default KegList;
