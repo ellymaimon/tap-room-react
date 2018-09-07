163f578d4cf4145910a5232ae640be756d7d662f
ellymaimon <eliranmaimon@gmail.com>
2018-09-07 14:27:48 -0700

    Add styling to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Footer(){
+  var link = {
+    textDecoration: "none",
+    color: "white",
+    marginLeft: "50px",
+    fontSize: "24px"
+  }
   return (
-    <div>
-      <Link to="/admin">Admin</Link>
+    <div className="nav">
+     <style jsx>{`
+        h1 {
+          text-align: center;
+        }
+        .nav {
+          height: 80px;
+          background-color: #505050;
+          color: white;
+          display: flex;
+          align-items: center;
+          border-bottom: 5px solid crimson;
+        }
+      `}</style>
+
+      <Link style={link} to="/admin">Admin</Link>
     </div>
   );
 }
