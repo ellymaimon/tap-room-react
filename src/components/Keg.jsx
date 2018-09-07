68e736c66ee64f081536934d699b137d367a376d
ellymaimon <eliranmaimon@gmail.com>
2018-09-07 12:04:30 -0700

    Create keg component with proptypes and destructured props

---
+++ src/components/Keg.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props) {
+  const { name, brewer, description, abv, price, remaining } = props;
+  return(
+    <div>
+      <h2>{name} by {brewer}</h2>
+      <h3><em>{description}</em></h3>
+      <h4>ABV: {abv} | Price: {price}</h4>
+      <p>There are {remaining} pints remaining.</p>
+      <hr/>
+    </div>
+  )
+}
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brewer: PropTypes.string,
+  description: PropTypes.string,
+  abv: PropTypes.string,
+  price: PropTypes.string,
+  remaining: PropTypes.string
+};
+
+export default Keg;
