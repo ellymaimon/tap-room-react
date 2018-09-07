7f6a73170f2a3f41ed07e10a982ec69aabafb606
ellymaimon <eliranmaimon@gmail.com>
2018-09-07 08:21:48 -0700

    Update app.jsx to show working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Error404 from './Error404';
+// import Error404 from './Error404';
 import { Switch, Route } from "react-router-dom";
 
 function App(){
   return (
     <div>
-      <Header/>
+      <p>App works!</p>
       {/* <Switch>
         <Route exact path='/' component={} />
         <Route path='/someroute' component={} />
