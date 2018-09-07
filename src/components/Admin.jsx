d3d6cdc17d62a1752cc5249441527fb946bc396b
ellymaimon <eliranmaimon@gmail.com>
2018-09-07 12:14:54 -0700

    Add admin component, render Keglist in admin component

---
+++ src/components/Admin.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import KegList from './KegList';
+
+function Admin(){
+  return (
+    <div>
+      <h1>Welcome, employee!</h1>
+      <KegList/>
+    </div>
+  );
+}
+
+export default Admin;
