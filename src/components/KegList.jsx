e2e497b5295f7d839b21e0ce4a9b046b4b424873
ellymaimon <eliranmaimon@gmail.com>
2018-09-07 14:23:14 -0700

    Add margin and max width to Keg List component

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -3,9 +3,15 @@ import { KEGLIST } from './MockData';
 import Keg from './Keg';
 
 function KegList() {
-  
   return (
     <div>
+      <style jsx>{`
+        div {
+          max-width: 1200px;
+          margin: 0 auto;
+        }
+      `}
+      </style>
       {KEGLIST.masterKegList.map((keg, index) =>
         <Keg {...keg} key={index} />
       )}
