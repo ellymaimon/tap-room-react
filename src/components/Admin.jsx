dc59d3f33122dec67f88a77c1c5d68c6d05ec125
ellymaimon <eliranmaimon@gmail.com>
2018-09-07 14:35:28 -0700

    Add max width and margin to admin component

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -5,8 +5,14 @@ import { Link } from "react-router-dom";
 function Admin(){
   return (
     <div>
+      <style jsx>{`
+        div {
+          max-width: 1200px;
+          margin: 0 auto;
+        }
+      `}</style>
       <h2>Welcome, employee!</h2>
-      <Link Link to="/newkeg"><button>Add Keg</button></Link>
+      <Link to="/newkeg"><button>Add Keg</button></Link>
       <h1>Kegs Available:</h1>
       <KegList/>
     </div>
