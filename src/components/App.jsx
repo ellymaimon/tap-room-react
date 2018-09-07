cd1b9d027e2fa09da2191adbfb06c12c15112c94
ellymaimon <eliranmaimon@gmail.com>
2018-09-07 11:01:42 -0700

    Add header to app.jsx, remove unnecessary element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from './Header';
 import KegList from './KegList';
 // import Error404 from './Error404';
 import { Switch, Route } from "react-router-dom";
@@ -6,7 +7,7 @@ import { Switch, Route } from "react-router-dom";
 function App(){
   return (
     <div>
-      <p>App works!</p>
+      <Header/>
       <Switch>
         <Route exact path='/' component={KegList} />
         {/* <Route path='/someroute' component={} />
