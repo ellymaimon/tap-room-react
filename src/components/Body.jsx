c901603225a66a6850e6ac17f94d6fd244b4a2cf
ellymaimon <eliranmaimon@gmail.com>
2018-09-14 12:22:30 -0700

    Refactor css objects in body component, remove button from link

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,29 +3,31 @@ import PropTypes from 'prop-types';
 import KegList from './KegList';
 import { Link } from "react-router-dom";
 
-const adminWelcomeContainer = {
-  display: "flex",
-  border: "2px solid #844949",
-  maxWidth: "600px",
-  margin: "0 auto",
-  justifyContent: "space-around",
-  alignItems: "center",
-  borderRadius: "25px"
-}
-
-const bodyContainer = {
-  maxWidth: "1200px",
-  margin: "0 auto"
-}
-
 const Body = (props) => {
+  const adminWelcomeContainer = {
+    display: "flex",
+    border: "2px solid #844949",
+    maxWidth: "600px",
+    margin: "0 auto",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderRadius: "25px"
+  }
+  
+  const bodyContainer = {
+    maxWidth: "1200px",
+    margin: "0 auto"
+  }
+
   let adminView = null;
+
   if(props.isAdmin) {
     adminView = <div style={adminWelcomeContainer}>
                   <h2>Welcome, employee!</h2>
-                  <button><Link to="/newkeg">Add Keg</Link></button>
+                  <Link to="/newkeg">Add Keg</Link>
                 </div>
   }
+
   return (
     <div style={bodyContainer}>
       <h1>What's On Tap?</h1>
