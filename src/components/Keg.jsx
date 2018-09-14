2bdef5f6cb8697f399479fdf75c07116c2fc9257
ellymaimon <eliranmaimon@gmail.com>
2018-09-14 09:55:20 -0700

    Add conditional rendering of edit button based on admin route, add prop type

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -3,25 +3,30 @@ import PropTypes from 'prop-types';
 
 function Keg(props) {
   const { name, brewer, description, abv, price, remaining } = props;
+  let adminView = null;
+  if (props.isAdmin) {
+    adminView = <button>Edit Keg (will only be available if on admin page)</button>;
+  }
   return(
     <div>
       <h2>{name} by {brewer}</h2>
       <h3><em>{description}</em></h3>
       <h4>ABV: {abv} | Price: {price}</h4>
       <p>There are {remaining} pints remaining.</p>
-      <button>Edit Keg (will only be available if on admin page)</button>
+      {adminView}
       <hr/>
     </div>
   )
 }
 
 Keg.propTypes = {
-  name: PropTypes.string,
-  brewer: PropTypes.string,
-  description: PropTypes.string,
-  abv: PropTypes.string,
-  price: PropTypes.string,
-  remaining: PropTypes.string
+  name: PropTypes.string.isRequired,
+  brewer: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  abv: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  remaining: PropTypes.string.isRequired,
+  isAdmin: PropTypes.bool.isRequired,
 };
 
 export default Keg;
