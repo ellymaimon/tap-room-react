7e245639de44cd66184f8b3b4bc80bea0f5c8084
ellymaimon <eliranmaimon@gmail.com>
2018-09-14 15:47:57 -0700

    Add automatic redirect to home if no keg selected but edit keg form route is reached

--- src/components/EditKegForm.jsx
+++ src/components/EditKegForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 const EditKegForm = (props) => {
 
@@ -15,33 +16,38 @@ const EditKegForm = (props) => {
     borderRadius: "25px"
   }
 
-  return (
-    <div>
-      <form style={formStyle}>
-        <div style={formGroupStyle}>
-          <label>New Name:</label>
-          <input type="text" name="name" placeholder={`Old name: ${props.location.state.kegName}`} />
-        </div>
-        <div style={formGroupStyle}>
-          <label>New Brewer:</label>
-          <input type="text" name="brewer" placeholder={`Old brewer: ${props.location.state.kegBrewer}`}/>
-        </div>
-        <div style={formGroupStyle}>
-          <label>New Description:</label>
-          <input type="text" name="description" placeholder={`Old description: ${props.location.state.kegDescription}`}/>
-        </div>
-        <div style={formGroupStyle}>
-          <label>New ABV:</label>
-          <input type="text" name="abv" placeholder={`Old ABV: ${props.location.state.kegAbv}`}/>
-        </div>
-        <div style={formGroupStyle}>
-          <label>New Price:</label>
-          <input type="text" name="price" placeholder={`Old price: ${props.location.state.kegPrice}`}/>
-        </div>
-        <button className="btn" type="submit">Finish Editing Keg</button>
-      </form>      
-    </div>
-  )
+  if(props.location.state === undefined) {
+    return <Redirect to="/" />
+  }
+  else {
+    return (
+      <div>
+        <form style={formStyle}>
+          <div style={formGroupStyle}>
+            <label>New Name:</label>
+            <input type="text" name="name" placeholder={`Old name: ${props.location.state.kegName}`} />
+          </div>
+          <div style={formGroupStyle}>
+            <label>New Brewer:</label>
+            <input type="text" name="brewer" placeholder={`Old brewer: ${props.location.state.kegBrewer}`}/>
+          </div>
+          <div style={formGroupStyle}>
+            <label>New Description:</label>
+            <input type="text" name="description" placeholder={`Old description: ${props.location.state.kegDescription}`}/>
+          </div>
+          <div style={formGroupStyle}>
+            <label>New ABV:</label>
+            <input type="text" name="abv" placeholder={`Old ABV: ${props.location.state.kegAbv}`}/>
+          </div>
+          <div style={formGroupStyle}>
+            <label>New Price:</label>
+            <input type="text" name="price" placeholder={`Old price: ${props.location.state.kegPrice}`}/>
+          </div>
+          <button className="btn" type="submit">Finish Editing Keg</button>
+        </form>      
+      </div>
+    )
+  }
 }
 
 export default EditKegForm;
