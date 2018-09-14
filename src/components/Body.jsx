0d00734f392b4f3b7e2594dc555b6d668fcc9901
ellymaimon <eliranmaimon@gmail.com>
2018-09-14 11:58:16 -0700

    Add styles to body, add conditional rendering of admin inside body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import KegList from './KegList';
+import { Link } from "react-router-dom";
+
+const adminWelcomeContainer = {
+  display: "flex",
+  border: "2px solid #844949",
+  maxWidth: "600px",
+  margin: "0 auto",
+  justifyContent: "space-around",
+  alignItems: "center",
+  borderRadius: "25px"
+}
+
+const bodyContainer = {
+  maxWidth: "1200px",
+  margin: "0 auto"
+}
 
 const Body = (props) => {
+  let adminView = null;
+  if(props.isAdmin) {
+    adminView = <div style={adminWelcomeContainer}>
+                  <h2>Welcome, employee!</h2>
+                  <button><Link to="/newkeg">Add Keg</Link></button>
+                </div>
+  }
   return (
-    <div>
-      <style jsx>{`
-        div {
-          max-width: 1200px;
-          margin: 0 auto;
-        }
-      `}
-      </style>
+    <div style={bodyContainer}>
       <h1>What's On Tap?</h1>
-      <KegList />
+      {adminView}
+      <KegList isAdmin={props.isAdmin} />
     </div>
-  )
+  );
 }
 
 Body.propTypes = {
