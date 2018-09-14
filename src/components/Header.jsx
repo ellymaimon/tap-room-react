e5f82faf8bee2afc61a2c70b7f86c8d4df8cd864
ellymaimon <eliranmaimon@gmail.com>
2018-09-14 12:28:59 -0700

    Refactor styling in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Header(){
-  var link = {
-    textDecoration: "none",
-    color: "white",
-    marginLeft: "50px",
-    fontSize: "22px"
+function Header() {
+  const headerContainer = {
+    backgroundColor: "#505050",
+    borderBottom: "4px solid goldenrod",
+    marginBottom: "25px",
+    padding: "25px"
   }
+
+  const headerStyle = {
+    textAlign: "center"
+  }
+
+  const linkStyle = {
+    color: "white",
+    fontSize: "1.2em"
+  }
+
   return (
     <div>
-      <style jsx>{`
-        h1 {
-          text-align: center;
-        }
-        .nav {
-          height: 60px;
-          background-color: #505050;
-          color: white;
-          display: flex;
-          align-items: center;
-          border-bottom: 4px solid goldenrod;
-          margin-bottom: 25px;
-        }
-      `}</style>
-
-      <h1>🍺 React Tap Room 🍺</h1>
-      <div className="nav">
-        <Link style={link} to="/">Home</Link>
+      <h1 style={headerStyle}>🍺 React Tap Room 🍺</h1>
+      <div style={headerContainer}>
+        <Link style={linkStyle} to="/">Home</Link>
       </div>
     </div>
   );
